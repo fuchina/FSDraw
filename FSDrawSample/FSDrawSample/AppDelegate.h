600a659ca4b883ef2d41bbf3b5601ad02b3f64cc
扶冬冬 <fudongdong@meituan.com>
2018-11-23 10:45:17 +0800

    创建测试工程

---
+++ FSDrawSample/FSDrawSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSDrawSample
+//
+//  Created by fudongdong on 2018/11/23.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSDrawSample/FSDrawSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FSDrawSample
+//
+//  Created by fudongdong on 2018/11/23.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
